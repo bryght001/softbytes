6f06c9bbdfa431869381440911489d4770c17df1
bryght001 <brightchukwunwike@gmail.com>
2024-08-30 16:50:56 +0100

    bryght

---
+++ layout/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lanh="en_US">
+<meta name="viewport" content="width=device-width, iniial-scale=1.0">
+<meta name="author" content="bryte">
+<meta name="description" content="Full digital marketing and software development agency">
+
+<head>
+  <title>Welcome to Softbytes: Your Software Solutions</title>
+  <link rel="stylesheet" href="" />
+
+</head>
+
+<body>
+
+</html>
