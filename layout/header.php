ba8d0820cb87e94b43b3f37fcf15e52a21fbb407
bryght001 <brightchukwunwike@gmail.com>
2024-08-31 06:34:05 +0100

    bryght

--- layout/header.php
+++ layout/header.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
-<html lanh="en_US">
+<html lang="en-US">
 <meta name="viewport" content="width=device-width, iniial-scale=1.0">
 <meta name="author" content="bryte">
 <meta name="description" content="Full digital marketing and software development agency">
 
 <head>
   <title>Welcome to Softbytes: Your Software Solutions</title>
+  <!---css links for softbytes begins at this point-->
   <link rel="stylesheet" href="" />
-
+  <link rel="stylesheet" href="" />
+  <link rel="stylesheet" href="" />
+  <!---favicom for softbytes-->
+  <link rel="favicom" href="fabicon.ico" />
 </head>
 
 <body>
