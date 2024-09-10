c9e02139fbc1940b9a6e9802e46bc3dbeedeb8af
bryght001 <brightchukwunwike@gmail.com>
2024-09-10 23:56:25 +0100

    bryght

--- layout/header.php
+++ layout/header.php
@@ -7,6 +7,9 @@
   <!---css links for softbytes begins at this point-->
   <link rel="stylesheet" href="assets/css/main.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <!---favicom for softbytes-->
   <link rel="favicom" href="favicon.ico" />
 </head>
