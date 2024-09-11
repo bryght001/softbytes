f8d434191f2fdc9fe23c4b63bbcde927f616a604
bryght001 <brightchukwunwike@gmail.com>
2024-09-12 00:26:57 +0100

    bryght

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <section clasd="aboutus" id="aboutus">
   <h1 class="heroHeader" id="heroHeader">Discover. Design. Develop</h1>
 
-  <p id="para1">Lets bring your vision to life</p>
-  <p id="para2">Transform your digital presence</p>
-  <p id="para3">At SoftBytes, we craft inmovative websites and software solutions that drives business success. Our experts blend creativity, technology and strategy to elevate your brands online footprint.</p>
+  <p id="heroParagraph1">Lets bring your vision to life</p>
+  <p id="heroParagraph2">Transform your digital presence</p>
+  <p id="heroParagraph3">At SoftBytes, we craft inmovative websites and software solutions that drives business success. Our experts blend creativity, technology and strategy to elevate your brands online footprint.</p>
 </section>
 
 
